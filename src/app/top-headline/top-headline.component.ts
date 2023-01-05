27218618ca719400f4cc5f2f8b1388041be83efa
Saurabh Kumar <saurabhkumar8298@gmail.com>
2023-01-05 23:51:46 +0530

    top news compoenet

--- src/app/top-headline/top-headline.component.ts
+++ src/app/top-headline/top-headline.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkyNewsServiceService } from '../service/sky-news-service.service';
 
 @Component({
   selector: 'app-top-headline',
   templateUrl: './top-headline.component.html',
   styleUrls: ['./top-headline.component.css']
 })
-export class TopHeadlineComponent {
-
+export class TopHeadlineComponent implements OnInit {
+  topNews:any =[];
+       constructor(private skyNewsService:SkyNewsServiceService){}
+ngOnInit(){
+     this.skyNewsService.getTopNewsUrl().subscribe(result=>{
+      this.topNews=result.articles;
+      
+     })
+}
 }
